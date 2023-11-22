61035066a5538d482df1d40acf13a331f0ef826c
Vainslav <139143272+Vainslav@users.noreply.github.com>
2023-11-22 09:53:26 +0300

    Update JsonToYaml.py

--- Lab4/JsonToYaml.py
+++ Lab4/JsonToYaml.py
@@ -1,20 +1,17 @@
 import time
 
 
-def to_yaml(dict_element, file, spaces, prev):
+def to_yaml(dict_element, file, spaces):
     if isinstance(dict_element, str):
         file.write(' ' + dict_element + '\n')
     elif isinstance(dict_element, list):
         for i in dict_element:
-            to_yaml(i, file, spaces, 'list')
+            to_yaml(i, file, spaces)
     elif isinstance(dict_element, dict):
         file.write('\n')
         for i in dict_element.keys():
-            if not prev == 'list':
-                file.write(spaces * ' ' + i + ':')
-            else:
-                file.write((spaces - 2) * ' ' + '- ' + i + ':')
-            to_yaml(dict_element[i], file, spaces + 2, 'dict')
+            file.write(spaces * ' ' + i + ':')
+            to_yaml(dict_element[i], file, spaces + 2)
 
 
 start_time = time.time()
@@ -22,7 +19,7 @@ for i in range(1000):
     with open("timetable.yaml", "w", encoding="UTF-8") as W:
         with open("schedule.json", encoding="UTF-8") as F:
             N = eval(' '.join(F.readlines()))
-            to_yaml(N, W, 0, 0)
+            to_yaml(N, W, 0)
 
 end_time = time.time()
 elapsed_time = end_time*100 - start_time*100
